0c106ecd9b16f2eb91be0285491cbb283f018937
jesusvaldezc <49350579+jesusvaldezc@users.noreply.github.com>
2021-09-05 00:57:55 -0500

    First commit

---
+++ functions.py
@@ -0,0 +1,9 @@
+import requests
+
+def download(url, file_name):
+    # open in binary mode
+    with open(file_name, "wb") as file:
+        # get request
+        response = requests.get(url)
+        # write to file
+        file.write(response.content)
