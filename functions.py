6438a7582cca28c08b345d177025b9680020d47c
jesusvaldezc <jesusv.c@hotmail.com>
2023-01-19 17:13:18 -0600

    Refactor Jupyter Notebook projects

--- functions.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def download(url, file_name):
-    # open in binary mode
-    with open(file_name, "wb") as file:
-        # get request
-        response = requests.get(url)
-        # write to file
-        file.write(response.content)
